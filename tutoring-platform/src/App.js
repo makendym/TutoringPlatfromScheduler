8e4fd02d2e7718368b7fafdfef1726633c839f3d
makendym <mmidouin18@stac.edu>
2022-12-13 22:38:51 -0500

    Updated a schedule form using a react scheduler library

--- tutoring-platform/src/App.js
+++ tutoring-platform/src/App.js
@@ -1,14 +1,14 @@
-
 import "./App.css";
 import Calendar from "./components/Calendar";
+import Schedule from "./components/Schedule";
 function App() {
   return (
     <div className="App">
       <div className="content">
-      <h1>Computer Science Tutoring</h1>
-      <p>Tutoring appointment scheduling</p>
-
-      </div> <Calendar /> 
+        <h1>Computer Science Tutoring</h1>
+        <p>Tutoring appointment scheduling</p>
+      </div>{" "}
+      <Schedule/>
     </div>
   );
 }
--- tutoring-platform/src/components/Calendar.js
+++ tutoring-platform/src/components/Calendar.js
@@ -17,13 +17,13 @@ import "../css/calendar.css";
 const Calendar = () => {
   const schedulerData = [
     {
-      startDate: "2022-11-11T09:45",
-      endDate: "2022-11-11T11:00",
+      startDate: "2022-12-16T09:45",
+      endDate: "2022-12-16T11:00",
       title: "Meeting",
     },
     {
-      startDate: "2022-11-11T12:00",
-      endDate: "2022-11-11T13:00",
+      startDate: "2022-12-16T12:00",
+      endDate: "2022-12-16T13:00",
       title: "Go to the gym",
     },
   ];
@@ -32,7 +32,52 @@ const Calendar = () => {
     console.log("Appointment Saved");
     console.log(data);
   };
+  const TextEditor = (props) => {
+    // eslint-disable-next-line react/destructuring-assignment
+    if (props.type === "multilineTextEditor") {
+      return null;
+    }
+    return <AppointmentForm.TextEditor {...props} />;
+  };
+  const divStyle = {
+    textAlign : "left",
+    
+  };
 
+  const BasicLayout = ({ onFieldChange, appointmentData, ...restProps }) => {
+    const onCustomFieldChange = (nextValue) => {
+      onFieldChange({ customField: nextValue });
+    };
+    const onCustomFieldChange2 = (nextValue) => {
+      onFieldChange({ customField2: nextValue });
+    };
+
+    return (
+      <AppointmentForm.BasicLayout
+        appointmentData={appointmentData}
+        onFieldChange={onFieldChange}
+        {...restProps}
+      >
+        <AppointmentForm.Label
+          text="First Name"
+          type="title"
+          style={divStyle}
+        />
+        <AppointmentForm.TextEditor
+          value={appointmentData.customField}
+          onValueChange={onCustomFieldChange}
+          placeholder="First Name"
+        />
+        <AppointmentForm.Label text="Last Name" type="title" style={divStyle}/>
+        <AppointmentForm.TextEditor
+          value={appointmentData.customField2}
+          onValueChange={onCustomFieldChange2}
+          placeholder="Last Name"
+        />
+      </AppointmentForm.BasicLayout>
+    );
+  };
+ 
   const views = ["week", "month"];
   return (
     <div id="calendar">
@@ -45,7 +90,10 @@ const Calendar = () => {
         <DateNavigator />
         <TodayButton />
         <Appointments />
-        <AppointmentForm />
+        <AppointmentForm
+          basicLayoutComponent={BasicLayout}
+          textEditorComponent={TextEditor}
+        />
       </Scheduler>
     </div>
   );
